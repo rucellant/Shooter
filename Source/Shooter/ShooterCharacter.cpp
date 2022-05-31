af91cce4bf967a2be7ad70bd5af87e49e3356844
rucellant <rucellant@gmail.com>
2022-05-31 15:54:50 +0900

    10. UE_LOG Format Specifiers

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -19,8 +19,28 @@ void AShooterCharacter::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("BeginPlay() called!"));
 
 	// {}->int를 제외한 다른 자료 스타일로 초기화하면 에러가 난다
+	// c++11부터 추가된 문법. 자료의 형변환을 막음. uniform initialization 혹은 brace initialization이라고 한다.
 	int myInt{ 42 };
 	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
+
+	float myFloat{ 3.14159f };
+	UE_LOG(LogTemp, Warning, TEXT("float myFloat: %f"), myFloat);
+
+	double myDouble{ 0.000756 };
+	UE_LOG(LogTemp, Warning, TEXT("double myDouble: %lf"), myDouble);
+
+	char myChar{ 'J' };
+	UE_LOG(LogTemp, Warning, TEXT("char myChar: %c"), myChar);
+
+	wchar_t wideChar{ L'J' };
+	UE_LOG(LogTemp, Warning, TEXT("wchar_t wideChar: %lc"), wideChar);
+
+	bool myBool{ true };
+	UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), myBool);
+
+	UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f, bool: %d"), myInt, myFloat, myBool);
+	// 매개변수 자리에 변수가 아니라 단순 데이터값을 넣어도 된다
+	// ex)UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), false);
 }
 
 // Called every frame
