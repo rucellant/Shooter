187e5186203f2712e3f66f330eefd256ca6fd272
rucellant <rucellant@gmail.com>
2022-05-31 16:28:42 +0900

    12. Camera Spring Arm

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -2,6 +2,7 @@
 
 
 #include "ShooterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
@@ -9,46 +10,21 @@ AShooterCharacter::AShooterCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create a camera boom (충돌이 있을 경우 캐릭터 쪽으로 끌어당깁니다.)
+	// 선언부에서 생성한 컴포넌트 포인터 변수는 구현부에서 CreateDefaultSubobject<>이용해서 실제 컴포넌트를 생성하고 해당 주소를 가리키게 된다.
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	// ACharacter를 상속한 액터들의 RootComponent는 무조건 CapsuleComponent다.
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 300.f;			// The camera follows at the distance behind the character
+	// bUsePawnControlRotation을 true로 설정하면 스프링암이 컨트롤러의 회전값을 따라간다.
+	CameraBoom->bUsePawnControlRotation = true;		// Rotate the arm based on the controller
 }
 
 // Called when the game starts or when spawned
 void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	UE_LOG(LogTemp, Warning, TEXT("BeginPlay() called!"));
 
-	// {}->int를 제외한 다른 자료 스타일로 초기화하면 에러가 난다
-	// c++11부터 추가된 문법. 자료의 형변환을 막음. uniform initialization 혹은 brace initialization이라고 한다.
-	int myInt{ 42 };
-	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
-
-	float myFloat{ 3.14159f };
-	UE_LOG(LogTemp, Warning, TEXT("float myFloat: %f"), myFloat);
-
-	double myDouble{ 0.000756 };
-	UE_LOG(LogTemp, Warning, TEXT("double myDouble: %lf"), myDouble);
-
-	char myChar{ 'J' };
-	UE_LOG(LogTemp, Warning, TEXT("char myChar: %c"), myChar);
-
-	wchar_t wideChar{ L'J' };
-	UE_LOG(LogTemp, Warning, TEXT("wchar_t wideChar: %lc"), wideChar);
-
-	bool myBool{ true };
-	UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), myBool);
-
-	UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f, bool: %d"), myInt, myFloat, myBool);
-	// 매개변수 자리에 변수가 아니라 단순 데이터값을 넣어도 된다
-	// ex)UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), false);
-
-	// ue4에서는 스트링을 쓸 때 FString타입의 스트링을 쓴다.
-	// *myString는 const TCHAR*(배열임)를 반환함. 이 방식은 C스타일로 스트링을 사용한다.
-	FString myString{ TEXT("My String!!!!") };
-	UE_LOG(LogTemp, Warning, TEXT("FString myString: %s"), *myString);
-
-	// GetName()은 인스턴스의 이름을 반환하는 함수
-	UE_LOG(LogTemp, Warning, TEXT("Name of instance: %s"), *GetName());
 }
 
 // Called every frame
--- Source/Shooter/ShooterCharacter.h
+++ Source/Shooter/ShooterCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "ShooterCharacter.generated.h"
 
+class USpringArmComponent;
+
 UCLASS()
 class SHOOTER_API AShooterCharacter : public ACharacter
 {
@@ -26,4 +28,15 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	// VisibleAnywhere은 해당 변수를 디테일 패널(게임 안의 인스턴스)과 블루프린트 어디에서도 확인할 수 있다.
+	// BlueprintReadOnly는 블루프린트에서 작업할 때 Get으로 얻어와서 쓸 수 있지만 Set으로 해당 변수를 바꾸거나 할 수 없다.
+	// 접근 지시 제어자가 private인 변수인 경우 해당 변수를 블루프린트에서 접근하기 위해서는 메타 데이터가 필요하다.
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+public:
+	/** Returns CAmeraBoom subobject */
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 };
